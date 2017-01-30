c36125f86bb9573f73baa8c6e2aee72e89749207
loc.ngo <loc.ngo@baldpeak.com>
2017-01-29 22:39:09 -0800

    add sample code for POST/GET request

---
+++ src/groovy/bookstore/utils/ErrorHelper.groovy
@@ -0,0 +1,8 @@
+package bookstore.utils
+
+class ErrorHelper {
+	static ERROR_MESSAGES = ['UNKNOWN_ERROR': 'Unknown errors have occurred',
+						  	 'BOOK_VIEW_ERROR': 'Error viewing book details',
+						    ]
+	
+}
